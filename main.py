f533e910561148ece053645d9d296e75a6dfbbb8
marianblaha <marian.blaha@gmail.com>
2026-06-03 14:05:18 +0200

    generate html report added

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import fitz
 import tempfile
 import os
 import re
+from weasyprint import HTML
+from jinja2 import Environment, FileSystemLoader
+import json
 
 app = FastAPI(
     title="PDF SOH Updater",
@@ -294,3 +297,30 @@ async def pdf_text(
     return {
         "text": text
     }
+
+@app.post("/generate-report")
+async def generate_report(data: dict):
+
+    env = Environment(
+        loader=FileSystemLoader("templates")
+    )
+
+    template = env.get_template(
+        "report.html"
+    )
+
+    html = template.render(**data)
+
+    pdf = HTML(
+        string=html,
+        base_url="/app"
+    ).write_pdf()
+
+    return Response(
+        content=pdf,
+        media_type="application/pdf",
+        headers={
+            "Content-Disposition":
+            "attachment; filename=battery-report.pdf"
+        }
+    )
