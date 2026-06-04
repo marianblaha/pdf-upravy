131bd10e857e6b01a007a623910b9bfa97ab5298
marianblaha <marian.blaha@gmail.com>
2026-06-04 17:45:23 +0200

    oprava soc z pdf reportu

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def extract_pdf_data(text: str):
     return {
 
         # hlavný blok
-        "soc": find(r"SOC:(\d+\.\d+)"),
+        "soc": str(round(float(find(r"SOC:(\d+\.\d+)")), 1)),
         "packVoltage": find(r"Total voltage:(\d+\.\d+)"),
         "totalCurrent": find(r"Total current:([-\d\.]+)"),
 
