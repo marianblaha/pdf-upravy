62260b8aa660720002d44bc0b97f0f560c849dfd
marianblaha <marian.blaha@gmail.com>
2026-06-10 11:42:55 +0200

    uprava v zavere spojene SOH a balans a pridana veta pre zvysene opotrebenie

--- main.py
+++ main.py
@@ -389,7 +389,7 @@ async def preview():
         "packVoltage": "354.1",
         "maxCellVoltage": "3.692",
         "minCellVoltage": "3.685",
-        "cellDelta": "0.007",
+        "cellDelta": "0.027",
         "maxTemp": "25",
         "minTemp": "24",
         "tempDelta": "1.0",
